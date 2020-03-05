fd92f79f4963986dffe17104ada60236d8da87dc
wardDes <dar-el@outlook.com>
2020-03-04 18:10:16 -0800

    made straw DrumPad component

---
+++ src/components/DrumPad.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface DrumPadProps {
+    
+}
+
+const DrumPad: React.FC<DrumPadProps> = props => {
+    return (
+        <div></div>
+    )
+}
+
+export default DrumPad
