b23624f7e78d742012a1b910da5182a4ef26d6e8
wardDes <dar-el@outlook.com>
2020-03-05 18:30:07 -0800

    completed implementing PadBank component

--- src/components/PadBank.tsx
+++ src/components/PadBank.tsx
@@ -1,11 +1,20 @@
 import React, { ReactNode} from 'react'
 import DrumPad from '../components/DrumPad'
 
+interface Sound {
+    keyCode: number,
+    keyTrigger: string,
+    id: string,
+    url: string,
+  }
+
+  
+
 interface BankPadProps {
     power: boolean,
     updateDisplay(name: string): void,
     clipVolume: number,
-    currentPadBank: Object[]
+    currentPadBank: Array<Sound>
 }
 
 const PadBank: React.FC<BankPadProps> = props => {
@@ -13,16 +22,32 @@ const PadBank: React.FC<BankPadProps> = props => {
     props.power ? 
         padBank = props.currentPadBank.map((drumObj, i, padBankArr) => {
             return (
-                <DrumPad  />
+                <DrumPad  
+                    clipId = {padBankArr[i].id}
+                    clip = {padBankArr[i].url}
+                    keyTrigger = {padBankArr[i].keyTrigger}
+                    keyCode = {padBankArr[i].keyCode}
+                    updateDisplay={props.updateDisplay}
+                    power={props.power}
+                />
             )
         }) :
         padBank = props.currentPadBank.map((drumObj, i, padBankArr) => {
             return (
-                <DrumPad />
+                <DrumPad 
+                    clipId = {padBankArr[i].id}
+                    clip = {padBankArr[i].url}
+                    keyTrigger = {padBankArr[i].keyTrigger}
+                    keyCode = {padBankArr[i].keyCode}
+                    updateDisplay={props.updateDisplay}
+                    power={props.power}
+                />
             )
         })
     return(
-        <div></div>
+        <div className="pad-bank">
+            {padBank}
+        </div>
     )
 }
 
